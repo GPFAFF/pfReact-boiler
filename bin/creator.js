8316a0fedfeb651c1e89f6e0fe5058c1e7134475
Greg Pfaff <greg.pfaff@razorfish.com>
2018-11-12 17:27:11 -0600

    changing tests file

--- bin/creator.js
+++ bin/creator.js
@@ -30,7 +30,7 @@ const jestConfig = `"license": "ISC",
     "transform": {
       "^.+\\\\.js$": "babel-jest",
       "^.+\\\\.jsx$": "babel-jest",
-      "\\\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/*"
+      "\\\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/fileTransformer.js"
     }
   }`;
 
